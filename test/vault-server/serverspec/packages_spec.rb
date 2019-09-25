0978ad75f6b33f954046a084c8f14222dd380d67
gitadmin <gitadmin@curbstone.com>
2019-09-24 22:33:50 -0400

    ArchOps-0001: Refactored playbook

---
+++ test/vault-server/serverspec/packages_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe yumrepo('epel') do
+  it { should exist }
+  it { should be_enabled }
+end
+
+%w(git
+  openssh-clients
+  rsync
+  sysstat
+  sudo
+  tar
+  unzip
+  wget
+  unzip
+  bzip2
+  ).each do |pkg|
+   describe package "#{pkg}", :if => os[:arch] == 'x86_64' do
+     it { should be_installed }
+     end
+  end
